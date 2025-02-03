de897203b36512c51cc78079bd3a1666a3ebd6db
Ronald <rockmetal281@gmail.com>
2025-02-03 12:02:21 -0400

    material inicializado con el provider del home, app router cargado

---
+++ lib/src/app.dart
@@ -0,0 +1,36 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:provider/provider.dart';
+
+// Project imports:
+import 'package:adivina_numero/src/core/app/app_router.dart';
+import 'package:adivina_numero/src/di/init_gi.dart';
+import 'package:adivina_numero/src/features/home/presentation/providers/home_provider.dart';
+import 'package:adivina_numero/src/features/splash/presentation/pages/splash_page.dart';
+
+final class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+final class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: gi<HomeProvider>()),
+      ],
+      child: MaterialApp(
+        initialRoute: SplashPage.id,
+        onGenerateRoute: (settings) => AppRouter.router.matchRoute(
+          settings.name!,
+          routeSettings: settings,
+        ),
+      ),
+    );
+  }
+}
