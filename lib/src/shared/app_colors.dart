d196ddf364369fa496d07cc8dae473a94bd34be9
Ronald <rockmetal281@gmail.com>
2025-02-03 22:23:13 -0400

    modulo shared completado con los const/widgets/dialogs necesarios

---
+++ lib/src/shared/app_colors.dart
@@ -0,0 +1,18 @@
+// Dart imports:
+import 'dart:ui';
+
+final class AppColors {
+  const AppColors._();
+
+  /// Primary
+  static const primary = Color(0xFF414141);
+
+  /// `primary.withValues(alpha: .20)`
+  static final primaryhighlight = primary.withValues(alpha: .20);
+
+  /// `primary.withValues(alpha: .40)`
+  static final primarySplash = primary.withValues(alpha: .40);
+
+  /// Home
+  static const greyBackground = Color(0xFF2F2F2F);
+}
